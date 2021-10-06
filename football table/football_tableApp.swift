9b7655b9905f029f1ef2dc1ca3ac7f65429310a2
Phlin <flynnhome3@gmail.com>
2021-10-06 12:27:50 +0100

    Initial Commit

---
+++ football table/football_tableApp.swift
@@ -0,0 +1,17 @@
+//
+//  football_tableApp.swift
+//  football table
+//
+//  Created by Flynn Milton on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct football_tableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
